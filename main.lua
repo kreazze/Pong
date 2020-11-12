f3c07aa00730b7d2d14abd37985e081edb037863
BlankSystem <alejandrobbabilonia@iskolarngbayan.pup.edu.ph>
2020-11-12 15:28:20 +0800

    Window size and keypressed

--- main.lua
+++ main.lua
@@ -1,1 +1,25 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
+function love.load()
+    
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true,
+    })
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
